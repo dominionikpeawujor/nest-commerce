a69b18893588ac862922272ed81797de6305480b
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-03-07 11:36:59 +0100

    type: refactor
    Import mongoose module into the app module of the nest application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-// import {MongooseModule} from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule],
   controllers: [AppController],
   providers: [AppService],
 })
